bc78b13edef4a3d2b092ba20a12dce3995d0e1f6
kardzae <emmanuelpp21@gmail.com>
2023-04-06 16:08:11 -0500

    agregando funcionalidades

--- controllers/legapp.controllers.js
+++ controllers/legapp.controllers.js
@@ -43,20 +43,66 @@ export const getUser = async (req, res)=>{
     
 }
 /**
- * 
+ * Creacion de proveedor
  * @param {Object} req 
  * @param {Object} res 
- * @param {function} next 
+ * @param {Object} next 
  */
 export const createProvider = async (req, res)=>{
     //Destructuración de valores nombreProveedor y telefonoProveedor desde el objeto body
-    const {nombreProveedor, telefonoProveedor} = req.body;
+    const {nit, nombreProveedor, telefonoProveedor, direccion, nombreContactoProveedor} = req.body;
 
     //Ejecución de la consulta SQL a la BD desde la función query del pool de conexión
-    await pool.query("INSERT INTO proveedor(nombreProveedor, telefonoProveedor) VALUES( ? ,  ?)", [nombreProveedor, telefonoProveedor]);
+    await pool.
+        query("INSERT INTO proveedor(NIT, nombreProveedor, telefonoProveedor, direccion, nombreContactoProveedor) VALUES( ?, ?, ?, ?, ? )",
+        [nit, nombreProveedor, telefonoProveedor, direccion, nombreContactoProveedor]);
 
-    //mensaje del servidor en el caso de que se haya ejecutado correctamente el controlador
+    //mensaje del servidor en el caso de que se haya ejecutado correctamente el controlador para crear proveedores
     res.json({
         message: "Proveedor creado"
+    });
+}
+/**
+ * Creación de usuario 
+ * @param {Object} req 
+ * @param {Object} res 
+ */
+export const createUser = async (req, res)=>{
+    //Destructuración de valores idRol, nombre, email y password desde el objeto body del request
+    const {idRol, nombre, email, password} = req.body;
+
+    //Ejecución de consulta SQL a la BD desde la función query del objeto pool
+    await pool.query("INSERT INTO usuario(idRol, nombre, email, password, estado) VALUES(?, ?, ?, MD5(?), ?)",
+        [idRol, nombre, email, password, 1]);
+
+    res.json({
+        message: "Usuario creado"
+    });
+    
+}
+
+export const modifyStateUser = async (req, res)=>{
+    //Destructuración del valor estado desde el objeto body
+    const {idUsuario, estado} = req.body;
+
+    //Ejecución de consulta SQL a la BD por medio de la función query
+    await pool.query("UPDATE usuario SET estado = ? WHERE idUsuario = ?", [estado, idUsuario]);
+
+    //Mensaje del servidor
+    res.json({
+        message: "state of user modified"
+    });
+}
+
+export const updateProvider = async (req, res)=>{
+
+    const {id, nit, nombreProveedor, telefonoProveedor, direccion, nombreContactoProveedor} = req.body;
+
+    await pool.
+        query("UPDATE proveedor SET NIT = IFNULL(?, NIT), nombreProveedor = IFNULL(?, nombreProveedor), telefonoProveedor = IFNULL(?, telefonoProveedor), direccion = IFNULL(?, direccion), nombreContactoProveedor = IFNULL(?, nombreContactoProveedor) WHERE idProveedor = ?", 
+        [nit, nombreProveedor, telefonoProveedor, direccion, nombreContactoProveedor, id]);
+
+    res.json({
+        message: "Provider updated"
     });
 }
--- routes/legapp.routes.js
+++ routes/legapp.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getUsers, getUser, createProvider } from '../controllers/legapp.controllers.js';
+import { getUsers, getUser, createProvider, createUser, modifyStateUser, updateProvider } from '../controllers/legapp.controllers.js';
 import { validateUser } from '../middlewares/auth.js';
 //Llamando el objeto Router de express para dar ruta a los métodos
 const router = express.Router();
@@ -7,8 +7,19 @@ const router = express.Router();
 //Usando el método GET del objeto router
 router.get('/usuarios', getUsers);
 
+//Método post para validación de usuarios
 router.post('/getUser', validateUser, getUser);
 
+//Método post para creación de proveedor
 router.post('/createProvider', createProvider);
+
+//Método post para creación de usuarios
+router.post('/createUser', createUser);
+
+//Método patch para actualizar el estado del usuario
+router.patch('/modifyState', modifyStateUser);
+
+
+router.patch('/updateProvider', updateProvider);
 
 export default router;
